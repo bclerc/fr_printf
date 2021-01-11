0d3de5cd5630a8cc11ed71f14ac0e2aa8eebcc70
bclerc <bryce@stocklerc.fr>
2021-01-11 12:30:52 +0100

    UN 0 PUTAIN J ETAIS BLOQUE A CAUSE D UN 0 PUTAIN

--- sources/pf_address.c
+++ sources/pf_address.c
@@ -6,23 +6,23 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 12:38:17 by bclerc            #+#    #+#             */
-/*   Updated: 2021/01/11 11:54:31 by bclerc           ###   ########.fr       */
+/*   Updated: 2021/01/11 12:29:30 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void tohex(unsigned long address)
+void print_hex(intptr_t address)
 {
-	const char hex[] = "123456789abcdef";
-	
-	if (address < 16)
-		ft_putchar(hex[address]);
-	else
-	{
-		tohex(address / 16);
-		tohex(address % 16);
-	}
+	const char hex[] = "0123456789abcdef";
+	const int base = 16;
+
+	if (address < base)
+		ft_putstr("0x");
+	if (address >= base)
+		print_hex(address / base);	
+
+	ft_putchar(hex[address % base]);
 }
 
 int pf_address(t_flag *flag)
@@ -30,5 +30,7 @@ int pf_address(t_flag *flag)
 	void		*address;
 
 	address = va_arg(flag->flags, void*);	
-	tohex((unsigned long)address);
+	print_hex((intptr_t)address);
+
+	return (1);
 }
