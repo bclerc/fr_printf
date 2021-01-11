4eff1090898c58becd2c206a6f3588fac064fd15
bclerc <bryce@stocklerc.fr>
2021-01-11 12:31:46 +0100

    ok

--- sources/pf_address.c
+++ sources/pf_address.c
@@ -6,7 +6,7 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 12:38:17 by bclerc            #+#    #+#             */
-/*   Updated: 2021/01/11 12:29:30 by bclerc           ###   ########.fr       */
+/*   Updated: 2021/01/11 12:31:40 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 void print_hex(intptr_t address)
 {
 	const char hex[] = "0123456789abcdef";
-	const int base = 16;
-
-	if (address < base)
+	if (address < 16)
 		ft_putstr("0x");
-	if (address >= base)
-		print_hex(address / base);	
-
-	ft_putchar(hex[address % base]);
+	if (address >= 16)
+		print_hex(address / 16);	
+	ft_putchar(hex[address % 16]);
 }
 
 int pf_address(t_flag *flag)
